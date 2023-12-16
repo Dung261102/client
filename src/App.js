799abdbba781a75c78e7e86f67c52196973ade90
Dung261102 <20521211@gm.uit.edu.vn>
2023-12-16 16:45:41 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 
+// //Test
+// import { Connect } from "react-redux";
+// import { useDispatch } from "react-redux";
+
+// import * as actions from './redux/actions';
+
+
+// function App() {
+//   // const dispatch = useDispatch();
+
+//   // dispatch(actions.getPosts.getPostsRequent());
+
+
+//   return <p> Dung </p>;
+// }
+
+
+
+
+
+//cuối cùng
+import HomePage from './pages/HomePage';
 
 function App() {
-  return <p> This is Blog App</p>
+  return <HomePage />;
 }
 
 export default App;
