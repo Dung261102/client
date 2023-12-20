1853436864b682e63cfdf97396f08d46be577fe3
Dung261102 <20521211@gm.uit.edu.vn>
2023-12-20 15:24:48 +0700

    Update src/components/PostList/index.js

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import * as actions from '../../redux/actions';
