d1153f0d5771a45e8ae3b81100f5b34987165c85
Dung261102 <20521211@gm.uit.edu.vn>
2023-12-20 15:27:05 +0700

    src/redux/sagas/index.js

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,48 +1,3 @@
-// import { takeLatest, call, put } from 'redux-saga/effects';
-// import * as actions from '../actions';
-// import * as api from '../../api';
-
-// function* fetchPostsSaga(action) {
-//     try {
-//         const posts = yield call(api.fetchPosts);
-//         yield put(actions.getPosts.getPostsSuccess(posts.data));
-//     } catch (err) {
-//         console.error(err);
-//         yield put(actions.getPosts.getPostsFailure(err));
-//     }
-// }
-
-// function* createPostSaga(action) {
-//     try {
-//         const post = yield call(api.createPost, action.payload);
-//         yield put(actions.createPost.createPostSuccess(post.data));
-//     } catch (err) {
-//         console.error(err);
-//         yield put(actions.createPost.createPostFailure(err));
-//     }
-// }
-
-// function* updatePostSaga(action) {
-//     try {
-//         const updatedPost = yield call(api.updatePost, action.payload);
-//         yield put(actions.updatePost.updatePostSuccess(updatedPost.data));
-//     } catch (err) {
-//         console.error(err);
-//         yield put(actions.updatePost.updatePostFailure(err));
-//     }
-// }
-
-// function* mySaga() {
-//     yield takeLatest(actions.getPosts.getPostsRequest, fetchPostsSaga);
-//     yield takeLatest(actions.createPost.createPostRequest, createPostSaga);
-//     yield takeLatest(actions.updatePost.updatePostRequest, updatePostSaga);
-// }
-
-// // generator function ES6
-
-// export default mySaga;
-
-
 import { takeLatest, call, put } from 'redux-saga/effects';
 import * as actions from '../actions';
 import * as api from '../../api';
@@ -77,10 +32,24 @@ function* updatePostSaga(action) {
     }
 }
 
+//Xóa bài viết
+function* deletePostSaga(action) {
+    try {
+        const postId = action.payload;
+        yield call(api.deletePost, postId);
+        yield put(actions.deletePost.deletePostSuccess(postId));
+    } catch (err) {
+        console.error(err);
+        yield put(actions.deletePost.deletePostFailure(err));
+    }
+}
+
+
 function* mySaga() {
     yield takeLatest(actions.getPosts.getPostsRequest, fetchPostsSaga);
     yield takeLatest(actions.createPost.createPostRequest, createPostSaga);
     yield takeLatest(actions.updatePost.updatePostRequest, updatePostSaga);
+    yield takeLatest(actions.deletePost.deletePostRequest, deletePostSaga); // Thêm watcher cho action xóa bài viết
 }
 
 // generator function ES6
