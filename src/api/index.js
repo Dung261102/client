6b1b6a080d1e5dacd993925143a648945a0b70f7
Dung261102 <20521211@gm.uit.edu.vn>
2023-12-22 22:22:17 +0700

    Update src/api/index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const URL = 'http://localhost:5000';
 
+// const URL = 'https://blog-app-nd-server.onrender.com/';
+
 export const fetchPosts = () => axios.get(`${URL}/posts`);
 export const createPost = (payload) => axios.post(`${URL}/posts`, payload);
 export const updatePost = (payload) =>
